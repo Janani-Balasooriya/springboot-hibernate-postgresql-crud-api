3b408fe81cd3a53bb622d3ab21f8182d553b1114
Janani-Balasooriya <jananidev7@gmail.com>
2025-05-16 14:48:45 +0530

    Add Employee Controller

---
+++ src/main/java/com/janani/crud_rest_api/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.janani.crud_rest_api.controller;
+
+import com.janani.crud_rest_api.dto.EmployeeDTO;
+import com.janani.crud_rest_api.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    // Build Add Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO){
+        EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
+
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
